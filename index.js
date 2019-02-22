f534e413e597f366e0b0f3f711851b94020b31f4
Anatoliy Ornatovskyy <Anatoliy.Ornatovskyy@sizmek.com>
2019-02-22 11:33:54 +0200

    added button for addind new parameter field

--- index.js
+++ index.js
@@ -9,54 +9,65 @@ class App extends React.Component {
     port: "http://",
     domain: "",
     parameters: [
-      { id: 1, param: "", paramValue: ""},
-      { id: 2, param: "", paramValue: ""}
+      { id: 1, param: "", paramValue: "" },
+      { id: 2, param: "", paramValue: "" }
     ]
   };
-  constructor(props){
-      super(props);
+  constructor(props) {
+    super(props);
   }
 
-  handleChange =  event => {
+  handleChange = event => {
     const domain = event.target.value;
-    this.setState({ domain:  domain });
+    this.setState({ domain: domain });
   };
 
-  componentDidUpdate(prevProps){
-    let {port, domain, parameters , result} = this.state;
+  componentDidUpdate() {
+    let { port, domain, parameters, result } = this.state;
     let url = port + domain;
-    
-    if(parameters.length > 0){
-        url += "?"
-    } 
 
-    parameters.forEach(el=>{
-        if(el.param !=="" || el.paramValue !=="" )
-        url += el.param + "=" + el.paramValue + "&";
-    })
-    url = url.slice(0, -1)
-    if(url != result){
-        this.setState({result : url});
+    if (parameters.length > 0) {
+      url += "?";
     }
-   
 
-    
+    parameters.forEach(el => {
+      if (el.param !== "" || el.paramValue !== "")
+        if (el.param !== "" && el.paramValue === "") {
+          url += el.param + "&";
+        } else {
+          url += el.param + "=" + el.paramValue + "&";
+        }
+    });
+    url = url.slice(0, -1);
+    if (url != result) {
+      this.setState({ result: url });
+    }
   }
 
   onChangeParams = event => {
     const field_id = event.id;
     const param = event.param;
     const paramValue = event.paramValue;
-   
+
     let parameterss = this.state.parameters;
 
     parameterss.forEach(element => {
-       if(element.id == field_id && (element.param !=param || element.paramValue !=paramValue)){
-          element.param = param;
-          element.paramValue = paramValue;
-          this.setState({parameters: parameterss });
-       }
+      if (
+        element.id == field_id &&
+        (element.param != param || element.paramValue != paramValue)
+      ) {
+        element.param = param;
+        element.paramValue = paramValue;
+        this.setState({ parameters: parameterss });
+      }
     });
+  };
+
+  onAddParamClick = () => {
+    let parameterss = this.state.parameters;
+    let prevId = parameterss[parameterss.length - 1].id;
+    const newParam = [...parameterss, { id: prevId +=1, param: "", paramValue: "" }];
+    this.setState({ parameters: newParam });
   };
 
   render() {
@@ -69,6 +80,8 @@ class App extends React.Component {
           margin="normal"
           variant="outlined"
           fullWidth
+          multiline
+          rowsMax="6"
           value={this.state.result}
         />
         <br />
@@ -79,15 +92,22 @@ class App extends React.Component {
           onChange={this.handleChange}
           margin="normal"
           fullWidth
+          style={{ width: 500 }}
           value={this.state.domain}
         />
 
         {parameters.map(el => {
-            return <UrlParam key={el.id + ""} values={el} onChangeParam={this.onChangeParams} 
-         />
-        })
-
-        }
+          return (
+            <UrlParam
+              key={el.id + ""}
+              values={el}
+              onChangeParam={this.onChangeParams}
+            />
+          );
+        })}
+        <Button variant="outlined" onClick={this.onAddParamClick}>
+          Add param
+        </Button>
       </div>
     );
   }
@@ -97,12 +117,11 @@ class UrlParam extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        id:"",
-        param: "",
-        paramValue: ""
+      id: "",
+      param: "",
+      paramValue: ""
     };
-   
-    }
+  }
 
   componentDidUpdate() {
     const state = this.state;
@@ -112,19 +131,17 @@ class UrlParam extends React.Component {
   handleChangeParam = event => {
     const id = event.target.id;
     const value = event.target.value;
-    this.setState({ id:id,param: value });
+    this.setState({ id: id, param: value });
   };
 
   handleChangeValue = event => {
     const id = event.target.id;
     const value = event.target.value;
-
     this.setState({ id: id, paramValue: value });
   };
 
   render() {
-    let {id} = this.props.values;
-     // console.log(this.props.values)
+    let { id } = this.props.values;
     return (
       <div>
         <TextField
@@ -132,6 +149,7 @@ class UrlParam extends React.Component {
           label="param name"
           onChange={this.handleChangeParam}
           margin="normal"
+          style={{ marginRight: 10 }}
           value={this.state.param}
         />
         <TextField
